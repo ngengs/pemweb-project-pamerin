ea8d919e731df8b1d0eefcfe90e933167af3eee8
rizky Kharisma <ngeng.ngengs@gmail.com>
2016-04-14 17:04:05 +0700

    Add Feature Change URL to Link:
    It will handle http://example.com www.example.com https://example.com to link and clickable

--- project_application/helpers/prjct_text_helper.php
+++ project_application/helpers/prjct_text_helper.php
@@ -5,6 +5,7 @@ if ( ! function_exists('prjct_text_generate_html'))
     function prjct_text_generate_html($desc = NULL)
     {
     	$CI =& get_instance();
+		$desc = auto_link($desc, 'url');
     	$desc = preg_replace('/(^|\s)#(\w*[a-zA-Z_]+\w*)/', '\1<a href="'.base_url().$CI->config->item('url_hashtags').'\2">#\2</a>', $desc);	
 		$desc = preg_replace('/(^|\s)@(\w*[a-zA-Z_]+\w*)/', '\1<a href="'.base_url().$CI->config->item('url_profile').'\2">@\2</a>', $desc);	
 		return nl2br($desc);
