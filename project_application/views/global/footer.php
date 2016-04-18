8f22d651d737814211c7fa5f431d2be3e56c03ef
rizky Kharisma <ngeng.ngengs@gmail.com>
2016-04-18 21:36:08 +0700

    [3/3] Add Footer
    Add footer item in view

--- project_application/views/global/footer.php
+++ project_application/views/global/footer.php
@@ -7,7 +7,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     
     <!-- Footer -->
     <footer id="footer">
-    	
+    	<div class="container">
+    		<div class="row">
+    			<div class="col-md-6 text-center text-left-md">
+    				<h4 div class="title-footer margin-bottom-sm"><?php echo $this->config->item('web_name');?></h4>
+    				<div class="copyright small">
+    					Copyright &copy; <?php echo $this->config->item('copyright_year');?> <?php echo $this->config->item('copyright_holder');?>
+    				</div>
+    			</div>
+    			<div class="col-md-6 text-center text-right-md">
+    				<ul class="list-unstyled list-inline margin-top-sm margin-bottom-sm">
+    					<?php foreach ($this->config->item('footer_menu') as $key => $value) { ?>
+							<li><a href="<?php if(!empty($value)||$value!="#")echo base_url($value);else echo $value;?>"><?php echo $key;?></a></li>
+						<?php } ?>
+    				</ul>
+    			</div>
+    		</div>
+    	</div>
     </footer>
     <!-- End Footerf -->
     
