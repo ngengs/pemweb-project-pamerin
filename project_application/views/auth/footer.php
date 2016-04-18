9d10bd2085686ddc98f1441c820724536776bbe4
rizky Kharisma <ngeng.ngengs@gmail.com>
2016-04-18 20:04:12 +0700

    Change package name
    Change the package name like the project name, and because package name is changed we must change the assets file name too in header and footer

--- project_application/views/auth/footer.php
+++ project_application/views/auth/footer.php
@@ -15,7 +15,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script> -->
     <script src="<?php echo base_url();?>assets/plugins/jquery/jquery-1.12.1.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo base_url();?>assets/js/pemweb_project.min.js"></script>
+    <script src="<?php echo base_url();?>assets/js/pemweb_project_pamerin.min.js"></script>
     <script src="<?php echo base_url();?>assets/plugins/fileupload/js/plugins/canvas-to-blob.min.js"></script>
     <script src="<?php echo base_url();?>assets/plugins/fileupload/js/fileinput.min.js"></script>
     <script src="<?php echo base_url();?>assets/js/system.min.js"></script>
